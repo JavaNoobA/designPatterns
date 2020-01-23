58397f7604c1fcc0996e918149d0fb4e1a770303
zhaopengfei <1027926875@qq.com>
2020-01-23 08:43:46 +0800

    simple factory01

---
+++ src/main/java/com/erudev/design/simpleFactory/Main.java
@@ -0,0 +1,27 @@
+package com.erudev.design.simpleFactory;
+
+import java.io.Console;
+import java.util.Scanner;
+
+/**
+ * Created by eru on 2020/1/23.
+ */
+public class Main {
+    public static void main(String[] args) {
+        double numA = 0d;
+        double numB = 0d;
+        String operator = null;
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+        while (scanner.hasNext()){
+            numA = Double.parseDouble(scanner.next());
+            numB = Double.parseDouble(scanner.next());
+            operator = scanner.next();
+            if ("quit".equals(scanner.next())){
+                break;
+            }
+        }
+        System.out.println("计算结果为 = " + Operation.getResult(numA, numB, operator));
+    }
+}
---
+++ src/main/java/com/erudev/design/simpleFactory/Operation.java
@@ -0,0 +1,25 @@
+package com.erudev.design.simpleFactory;
+
+/**
+ * Created by eru on 2020/1/23.
+ */
+public class Operation {
+    public static double getResult(double numA, double numB, String operator){
+        double result = 0d;
+        switch (operator){
+            case "+":
+                result = numA + numB;
+                break;
+            case "-":
+                result = numA - numB;
+                break;
+            case "*":
+                result = numA * numB;
+                break;
+            case "/":
+                result = numA / numB;
+                break;
+        }
+        return result;
+    }
+}
